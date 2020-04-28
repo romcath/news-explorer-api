0339dcfbbf6bbcd58e57dccfd2d249c570aeaa92
Kate <54095868+romcath@users.noreply.github.com>
2020-04-28 22:38:36 +0300

    Created 'deleteArticleID' controller

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,5 +1,8 @@
 const Article = require('../models/article');
 
+const NotFoundError = require('../errors/not-found-err');
+const ForbiddenError = require('../errors/forbidden-err');
+
 // Возвращает все сохранённые пользователем статьи
 const getAllArticles = (req, res, next) => {
   Article.find({ owner: req.user._id })
@@ -16,7 +19,22 @@ const createArticle = (req, res, next) => {
     .catch(next);
 };
 
+// Удаляет сохранённую статью по _id
+const deleteArticleID = (req, res, next) => {
+  Article.findById(req.params.articleId).select('+owner')
+    .orFail(new NotFoundError(`Нет статьи с id ${req.params.articleId}`))
+    .then(article => {
+      if (!article.owner.equals(req.user._id)) {
+        throw new ForbiddenError('Вы не можете удалить статью, которую не сохраняли');
+      }
+      return Article.deleteOne(article)
+        .then(() => res.send(article));
+    })
+    .catch(next);
+};
+
 module.exports = {
   createArticle,
   getAllArticles,
+  deleteArticleID,
 };
--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,10 @@
 const route = require('express').Router();
 
-const { createArticle, getAllArticles } = require('../controllers/articles');
-const { articleValidation } = require('../middlewares/article-validation');
+const { createArticle, getAllArticles, deleteArticleID } = require('../controllers/articles');
+const { articleValidation, articleIdValidation } = require('../middlewares/article-validation');
 
 route.get('/articles', getAllArticles);
 route.post('/articles', articleValidation, createArticle);
+route.delete('/articles/:articleId', articleIdValidation, deleteArticleID);
 
 module.exports = route;
