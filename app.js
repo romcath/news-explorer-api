32d4c129988c2f78993f959c47c500c48197a99c
Kate <54095868+romcath@users.noreply.github.com>
2020-05-21 14:43:52 +0300

    Added CORS

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const errorHandler = require('./middlewares/error-handler');
 const routes = require('./routes/index');
@@ -14,6 +15,7 @@ const { limiter } = require('./middlewares/rate-limiter');
 
 const app = express();
 
+app.use(cors(({origin:true, credentials: true})));
 app.use(helmet());
 app.use(limiter);
 app.use(cookieParser());
--- controllers/users.js
+++ controllers/users.js
@@ -41,7 +41,8 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, SECRET);
-      res.cookie('jwt', token, { maxAge: LIFETIME_COOKIES, httpOnly: true, sameSite: true }).end();
+      res.cookie('jwt', token, { maxAge: LIFETIME_COOKIES, httpOnly: true});
+      res.send({ token });
     })
     .catch(next);
 };
