bddaec73b3d10fe0ab5c1330f01694a011794796
Kate <54095868+romcath@users.noreply.github.com>
2020-05-30 22:12:12 +0300

    Updated Сors options

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const app = express();
 app.use(cors(({
   origin: [
     'http://localhost:8080',
+    'https://news-app.cf',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
